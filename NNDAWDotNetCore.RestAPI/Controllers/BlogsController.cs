13fc3e7030251ccc9bb2c74f30f9152b70f0983f
ace <ace@DESKTOP-TC3634B>
2024-10-11 18:45:55 +0630

    feat: BlogsController

---
+++ NNDAWDotNetCore.RestAPI/Controllers/BlogsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NNDAWDotNetCore.RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+    }
+}
