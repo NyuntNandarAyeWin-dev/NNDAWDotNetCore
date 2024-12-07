3acbaf9f30e8800fbe0f3711165806ca9df93f9f
ace <ace@DESKTOP-TC3634B>
2024-12-07 23:56:51 +0630

    feat : json to c# and c# to json

--- NNDAWDotNetCore.ConsoleApp2/Program.cs
+++ NNDAWDotNetCore.ConsoleApp2/Program.cs
@@ -1,16 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
 using NNDAWDotNetCore.Database.Models;
 using System;
+using System.Net.Http.Headers;
 
 Console.WriteLine("Hello, World!");
 
-AppDbContext db = new AppDbContext();
-var lst = db.TblBlogs.ToList();
+//AppDbContext db = new AppDbContext();
+//var lst = db.TblBlogs.ToList();
 
-foreach(var item in lst)
+//foreach(var item in lst)
+//{
+//    Console.WriteLine(item.BlogId);
+//    Console.WriteLine(item.BlogTitle);
+//    Console.WriteLine(item.BlogAuthor);
+//    Console.WriteLine(item.BlogContent);
+//}
+
+var blog = new BlogModel
 {
-    Console.WriteLine(item.BlogId);
-    Console.WriteLine(item.BlogTitle);
-    Console.WriteLine(item.BlogAuthor);
-    Console.WriteLine(item.BlogContent);
+    Id = 1,
+    Title = "Test Title",
+    Author = "Test Author",
+    Content = "Test Contest",
+};
+
+// Encode, Decode, Encryption, Decryption
+
+// Encode , Encryption => SerializeObject { C# to Json }
+// Decode , Decryption => DeserializeObject { Json to C# }
+
+//string jsonStr = JsonConvert.SerializeObject(blog, Formatting.Indented);
+string jsonStr = blog.ToJson();
+
+Console.WriteLine(jsonStr);
+
+string jsonStr2 = "{\"Id\":1,\"Title\":\"Test Title\",\"Author\":\"Test Author\",\"Content\":\"Test Contest\"}";
+var blog2 = JsonConvert.DeserializeObject<BlogModel>(jsonStr2);
+Console.WriteLine(blog2.Title);
+
+//System.Text.Json.JsonSerializer.Serialize(jsonStr); => case sensitive
+//System.Text.Json.JsonSerializer.Deserialize<BlogModel>(jsonStr2); => case sensitive
+
+Console.ReadLine();
+
+public class BlogModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Content { get; set; }
+}
+
+public static class Extensions //Dev Code
+{
+    public static string ToJson(this object obj)
+    {
+        string jsonStr = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        return jsonStr;
+    }
 }
