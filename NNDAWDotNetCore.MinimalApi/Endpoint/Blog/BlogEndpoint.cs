c099343070f0cc82b11545c9c094ad353ef0cad8
ace <ace@DESKTOP-TC3634B>
2024-12-07 23:56:10 +0630

    feat : minimal Api

---
+++ NNDAWDotNetCore.MinimalApi/Endpoint/Blog/BlogEndpoint.cs
@@ -0,0 +1,81 @@
+﻿namespace NNDAWDotNetCore.MinimalApi.Endpoint.Blog;
+
+public static class BlogEndpoint
+{
+    //public static string Test(int i)
+    //{
+    //    return i.ToString();
+    //}
+
+    public static void MapBlogEndPoint(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/blogs", () =>
+        {
+            AppDbContext db = new AppDbContext();
+            var model = db.TblBlogs.AsNoTracking().ToList();
+            return Results.Ok(model);
+        })
+        .WithName("GetBlogs")
+        .WithOpenApi();
+
+        app.MapGet("/blogs/{id}", (int id) =>
+        {
+            AppDbContext db = new AppDbContext();
+            var item = db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Results.BadRequest("No Data Found");
+            }
+
+            return Results.Ok(item);
+        })
+        .WithName("GetBlog")
+        .WithOpenApi();
+
+        app.MapPost("/blogs", (TblBlog blog) =>
+        {
+            AppDbContext db = new AppDbContext();
+            db.TblBlogs.Add(blog);
+            db.SaveChanges();
+            return Results.Ok();
+        })
+        .WithName("CreateBlog")
+        .WithOpenApi();
+
+        app.MapPut("/blogs/{id}", (int id, TblBlog blog) =>
+        {
+            AppDbContext db = new AppDbContext();
+            var item = db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Results.BadRequest("No Data Found");
+            }
+
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return Results.Ok();
+        })
+        .WithName("UpdateBlog")
+        .WithOpenApi();
+
+        app.MapDelete("/blogs/{id}", (int id) =>
+        {
+            AppDbContext db = new AppDbContext();
+            var item = db.TblBlogs.AsNoTracking().FirstOrDefault(x => x.BlogId == id);
+            if (item is null)
+            {
+                return Results.BadRequest("No Data Found");
+            }
+
+            db.Entry(item).State = EntityState.Deleted;
+            db.SaveChanges();
+            return Results.Ok();
+        })
+        .WithName("DeleteBlog")
+        .WithOpenApi();
+    }
+}
---
+++ NNDAWDotNetCore.MinimalApi/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+//BlogEndpoint.Test(10);
+//BlogEndpoint.Test("a");
+
+//10.Test();
+//"a".Test();
+
+app.MapBlogEndPoint();
+
+app.Run();
